cbd6ef11a4fb3ebdaa9df332a4e36344764c2130
Russell Sabia <rjsabia@gmail.com>
2017-09-26 14:27:52 -0500

    added background, dispatch and map props built, add and delete characters functioning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import CharacterList from './CharacterList';
+import HeroList from './HeroList';
+import '../styles/index.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
+			<div className="App">
 				<h2>Super Squad</h2>
+				<div className="col-md-6">
+					<CharacterList />
+				</div>
+				<div className="col-md-6">
+					<HeroList />
+				</div>
 			</div>
 		)
 	}
